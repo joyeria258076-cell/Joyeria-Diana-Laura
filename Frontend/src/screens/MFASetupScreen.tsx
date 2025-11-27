e4f5509126767b4fc55e16295377b226e5f5358a
Delfino114 <134705635+Delfino114@users.noreply.github.com>
2025-11-27 11:24:08 -0600

    Estilos-MFA

--- Frontend/src/screens/MFASetupScreen.tsx
+++ Frontend/src/screens/MFASetupScreen.tsx
@@ -1,3 +1,4 @@
+// Ruta: Frontend/src/screens/MFASetupScreen.tsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
